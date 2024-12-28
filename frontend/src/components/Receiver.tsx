ae063f78cea8876beaa1ed89606c20f03744a538
Om Sharma <omsharma.83173@gmail.com>
2024-12-28 15:39:43 +0530

    All-Features-Working.File-Share-Project-Complete-working-final

--- frontend/src/components/Receiver.tsx
+++ frontend/src/components/Receiver.tsx
@@ -85,11 +85,11 @@ export default function Receiver(){
 
             if(event.data  === "EOF"){
                 console.log("in the eof section")
-                const blob = new Blob(receivedChunks);
+                const blob = new Blob(receivedChunks,{type:fileType});
                 const url = URL.createObjectURL(blob);
                 const a = document.createElement("a");
                 a.href = url;
-                a.download = "received file";
+                a.download = fileName;
                 a.click();
 
             }else{
